e77d594d49f93c93f3dc0eed0b58a096e6f718f6
Andre <andre.spittel94@outlook.de>
2020-03-05 20:54:04 +0100

    more Preparation for export as package

--- Visualization Decision Trees/Assets/DT Visualizer/Scripts/ISplitrule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using ReadWriteCsv;
-using System;
-using System.Data;
-
-public interface ISplitrule
-{
-    bool Execute(DataRow row, string attribute, string split);
-}
